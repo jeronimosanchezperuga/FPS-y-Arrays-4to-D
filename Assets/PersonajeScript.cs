9ce8ef9c9f9fa86dc6e0b9ce9c869c209e76c4ad
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2024-06-06 12:18:25 -0300

    Initial commit

---
+++ Assets/PersonajeScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonajeScript : MonoBehaviour
+{
+    public int nivelHambre;
+
+   void TomarAlimento(int valorAlimentario)
+    {
+        nivelHambre -= valorAlimentario;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        AlimentoScript alimento;
+        alimento = other.GetComponent<AlimentoScript>();
+        if (alimento)
+        {
+            TomarAlimento(alimento.valorAlimentario);
+            Destroy(other.gameObject);
+        }
+    }
+}
